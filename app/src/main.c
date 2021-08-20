a834bc092a960248b4bf12b3d351b8406072616d
Brian Bradley <brian.bradley.p@gmail.com>
2021-08-20 11:52:56 -0400

    Add initial project template
    Signed-off-by: Brian Bradley <brian.bradley.p@gmail.com>

---
+++ app/src/main.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 LION
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include "app_version.h"
+
+#include <logging/log.h>
+#include <drivers/gpio.h>
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#else
+/* A build error here means your board isn't set up to blink an LED. */
+#error "Unsupported board: led0 devicetree alias is not defined"
+#define LED0	""
+#define PIN	0
+#define FLAGS	0
+#endif
+
+LOG_MODULE_REGISTER(main, CONFIG_PYRRHA_LOG_LEVEL);
+
+void main(void)
+{
+	const struct device *dev;
+	bool led_is_on = true;
+	int ret;
+
+	printk("Starting Pyrrha firmware version %s\n", APP_VERSION_STR);
+
+	dev = device_get_binding(LED0);
+	if (dev == NULL) {
+		return;
+	}
+
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		gpio_pin_set(dev, PIN, (int)led_is_on);
+		led_is_on = !led_is_on;
+		LOG_DBG("LED=%d", led_is_on);
+		k_msleep(1000);
+	}
+}
