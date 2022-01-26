e113d88417ac076fe375e90506663a5928c5337a
Brian Bradley <brian.bradley.p@gmail.com>
2022-01-26 14:30:49 -0500

    Add configurable thresholds for sensor data which will generate alert conditions.
    Signed-off-by: Brian Bradley <brian.bradley.p@gmail.com>

--- app/src/alerts.c
+++ app/src/alerts.c
@@ -13,6 +13,22 @@
 #include <alerts.h>
 #include <logging/log.h>
 LOG_MODULE_REGISTER(alerts, CONFIG_PYRRHA_LOG_LEVEL);
+#define OVER_TEMPERATURE(temp) (temp > CONFIG_TEMPERATURE_ALERT_THRESHOLD_CELCIUS)
+#define OVER_CO(co) (co > CONFIG_CO_ALERT_THRESHOLD_PPM)
+#define OVER_NO2(no2) (no2 > CONFIG_NO2_ALERT_THRESHOLD_PPM)
+
+/**
+ * @brief generate an alert from sensor data exceeding set thresholds
+ */
+static void threshold_alert(void){
+    return;
+}
 int generate_alerts(struct pyrrha_data * data){
+    if (OVER_TEMPERATURE(data->rht.temperature.val1) ||
+        OVER_CO(data->gas.co.val1) ||
+        OVER_NO2(data->gas.no2.val1))
+    {
+        threshold_alert();
+    }
     return 0;
 }
