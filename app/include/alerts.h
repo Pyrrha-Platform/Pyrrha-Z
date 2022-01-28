12860dd2fe89a855e5aadf1d6be2581e92d0ae57
Brian Bradley <brian.bradley.p@gmail.com>
2022-01-28 15:07:01 -0500

    Add an override method to the LED alert process so that a connected device can control the alert directly.
    Signed-off-by: Brian Bradley <brian.bradley.p@gmail.com>

--- app/include/alerts.h
+++ app/include/alerts.h
@@ -21,8 +21,28 @@
  * 
  * @param data : sensor data used to match alert conditions
  * @retval 0 on success
+ * @retval -ENOLCK if alert mutex could not be locked within 250ms
  * @retval -errno otherwise
  */
 int generate_alerts(struct pyrrha_data * data);
+
+/**
+ * @brief Override the current alert condition.
+ * This will block local alert generation until a release is issued.
+ * 
+ * @param color : hex representation of led color used in alarm
+ * @retval 0 on success
+ * @retval -ENOLCK if alert mutex could not be locked within 250ms
+ */
+int override_alert(uint32_t color);
+
+/**
+ * @brief Release the current override for the LED. For instance, 
+ * if the host device disconnects and can no longer control the alarm
+ * 
+ * @retval 0 on success
+ * @retval -ENOLCK if alert mutex could not be locked within 250ms
+ */
+int release_alert_override();
 
 #endif
--- app/src/alerts.c
+++ app/src/alerts.c
@@ -12,23 +12,76 @@
 
 #include <alerts.h>
 #include <logging/log.h>
+#include <zephyr.h>
 LOG_MODULE_REGISTER(alerts, CONFIG_PYRRHA_LOG_LEVEL);
 #define OVER_TEMPERATURE(temp) (temp > CONFIG_TEMPERATURE_ALERT_THRESHOLD_CELCIUS)
 #define OVER_CO(co) (co > CONFIG_CO_ALERT_THRESHOLD_PPM)
 #define OVER_NO2(no2) (no2 > CONFIG_NO2_ALERT_THRESHOLD_PPM)
 
 /**
+ * @brief union which allows easy set/conversion to hex or rgb directly
+ * 
+ */
+typedef union Color{
+    uint32_t hex;
+    struct{
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+    };
+}color_t;
+
+K_MUTEX_DEFINE(mtx_alert);
+static color_t g_alert_override = {0};
+
+/**
+ * @brief Set the alert led to a specific color
+ * 
+ * @param color : rgb representation of desired color 
+ * @retval 0 on success
+ * @retval -errno otherwise
+ */
+static int set_alert_led(color_t color){
+    return 0;
+}
+
+/**
  * @brief generate an alert from sensor data exceeding set thresholds
  */
 static void threshold_alert(void){
-    return;
+    const color_t alert_color = {
+        .red = 0xff
+    };
+    set_alert_led(alert_color);
 }
-int generate_alerts(struct pyrrha_data * data){
-    if (OVER_TEMPERATURE(data->rht.temperature.val1) ||
-        OVER_CO(data->gas.co.val1) ||
-        OVER_NO2(data->gas.no2.val1))
-    {
-        threshold_alert();
+
+int override_alert(uint32_t color){
+    if (k_mutex_lock(&mtx_alert, K_MSEC(250)) != 0){
+        return -ENOLCK;
     }
+    g_alert_override.hex = color;
+    set_alert_led(g_alert_override);
+    k_mutex_unlock(&mtx_alert);
+    return 0;
+}
+
+int release_alert_override(){
+    return override_alert(0);
+}
+
+int generate_alerts(struct pyrrha_data * data){
+    if (k_mutex_lock(&mtx_alert, K_MSEC(250)) != 0){
+        return -ENOLCK;
+    }
+    /* Only generate an alert when there is no override condition */
+    if (g_alert_override.hex == 0){
+        if (OVER_TEMPERATURE(data->rht.temperature.val1) ||
+            OVER_CO(data->gas.co.val1) ||
+            OVER_NO2(data->gas.no2.val1))
+        {
+            threshold_alert();
+        }
+    }
+    k_mutex_unlock(&mtx_alert);
     return 0;
 }
